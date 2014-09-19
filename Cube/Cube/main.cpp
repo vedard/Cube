05275654e0e0f659d16963946b21966d44a1e85a
Vincent Bédard <vedard@gmail.com>
2014-09-19 13:34:04 -0400

    Ajout de la classe BlocInfo

--- Cube/Cube/main.cpp
+++ Cube/Cube/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+#include "blocinfo.h"
+#include <iostream>
+#include <string>
+#include "define.h"
+
 int main()
 {
-
+	
+	BlocInfo test(BlocType::BTYPE_GRASS ,"Bloc de test");
+	test.Afficher();
 
 	system("pause");
 	return 0;
